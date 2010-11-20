b24790bfc65aed8f9d76c0dc089e9c4b1b30b225
narumug3 <narumug3@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-20 22:10:36 +0000

    E02: Created db script for voluntary_work for voluntary work credit project. - to handle the weightage for extra credit tasks
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1131 392d7b7b-3f31-0410-9dc3-c95a9635ea79

---
+++ db/migrate/20101120220132_create_voluntary_works.rb
@@ -0,0 +1,15 @@
+class CreateVoluntaryWorks < ActiveRecord::Migration
+  def self.up
+    create_table :voluntary_works do |t|
+      t.string :name
+      t.int :weight
+      t.string :type
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :voluntary_works
+  end
+end
