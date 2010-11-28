51ce606ad962f2483cbd2e12c1fdc9ef665fdac8
nkote <nkote@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-11-28 01:24:31 +0000

    E02: Credit for voluntary work : migration script for voluntary scores
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectA/trunk@1163 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- db/migrate/20101120220132_create_voluntary_works.rb
+++ db/migrate/20101120220132_create_voluntary_works.rb
@@ -3,10 +3,14 @@ class CreateVoluntaryWorks < ActiveRecord::Migration
     create_table :voluntary_works do |t|
       t.string :name
       t.float :weight
-      t.string :type
-
+      t.string :task_type
+      t.integer :course_id
+     
       t.timestamps
     end
+    execute "alter table voluntary_works
+             add constraint fk_course_voluntary_works
+             foreign key (course_id) references courses(id)"    
   end
 
   def self.down
