d5a821c78afee51a29802d322bd09bff4bd88286
kdlagu <kdlagu@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-01 07:53:16 +0000

    E09: Project B: Unit tests for Course Users
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectB/trunk@1213 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/courses_users_test.rb
+++ test/unit/courses_users_test.rb
@@ -1,11 +1,40 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class CoursesUsersTest < Test::Unit::TestCase
- # fixtures :courses_users
+class CoursesUsersTest < ActiveSupport::TestCase
+ fixtures :courses, :users
   #TODO verify if fixture is correct
   # Replace this with your real tests.
 # Need to add more tests
   def test_truth
     assert true
   end
+
+  def test_import
+    row = Array.new
+    row[0] = "s1"
+    row[1] = "Student, One"
+    row[2] = "one.student@blah.foo"
+    row[3] = "s1"
+
+    session = Hash.new
+    session[:user] = users(:superadmin)
+
+
+    #course_user = CoursesUsers.new
+    course = Course.new
+    course= courses(:course_nil)
+    id = course.id
+     #TODO after getting courses_users table
+    #cu = CoursesUsers.count
+    #CoursesUsers.import(row, session,id)
+    #assert_equal cu+1,CoursesUsers.count
+
+    id1 = nil
+    assert_raise(MissingObjectIDError){CoursesUsers.import(row, session,id1)}
+    #TODO after getting courses_users table
+    #assert_raise(ImportError){CoursesUsers.import(row,session,id)}
+    
+
+   end
 end
+
