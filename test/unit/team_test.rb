d1184a633270c463fc9618d66e6886a9e52c5b4b
lgalanr <lgalanr@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-03 23:11:21 +0000

    E09: Participant and Team unit tests
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectB/trunk@1275 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/team_test.rb
+++ test/unit/team_test.rb
@@ -11,16 +11,16 @@ class TeamTest < ActiveSupport::TestCase
   end
   
   def test_add_team_member
-    #team = teams(:team0)
-    currTeam = CourseTeam.new
     course = courses(:course0)
+    parent = CourseNode.create(:parent_id => nil, :node_object_id => course.id)
+    
+    currTeam = CourseTeam.new
    	currTeam.name = name
    	currTeam.parent_id = course.id
-   	
    	assert currTeam.save
 
-   	parent = CourseNode.create(:parent_id => nil, :node_object_id => course.id)
    	TeamNode.create(:parent_id => parent.id, :node_object_id => currTeam.id)
+   	#TODO assertion missing?
 
     currTeam.add_member(users(:student1));
     assert currTeam.has_user(users(:student1))
