9d4beaeba92be101e4600771af1c81c83d753b16
kdlagu <kdlagu@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-03 23:34:59 +0000

    E09: Project B - reverted metareview_questionnaire test
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectB/trunk@1278 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- test/unit/metareview_questionnaire_test.rb
+++ test/unit/metareview_questionnaire_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class MetareviewQuestionnaireTest < ActiveSupport::TestCase
- fixtures :metareview_questionnaires, :participants, :assignments, :questionnaires
+ #fixtures :metareview_questionnaires, :participants, :assignments, :questionnaires
   #TODO verify if correct fixture included
   #fixtures :questionnaires
   # Replace this with your real tests.
@@ -9,12 +9,6 @@ class MetareviewQuestionnaireTest < ActiveSupport::TestCase
     assert true
   end
 
- #TODO after getting meta_review_questionnaires table
 
-  def test_get_assessment_for
-     participant = Participant.new
-     participant = participants(:participant1)
-     mrq = MetaReviewQuestionnaire.new
-     mrq.get_assessments_for(participant)
-   end
+  
   end                                              
