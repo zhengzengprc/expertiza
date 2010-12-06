a8323ae3a0975868a183a49c21b3ed175f8b498b
mtreece <mtreece@392d7b7b-3f31-0410-9dc3-c95a9635ea79>
2010-12-06 21:55:35 +0000

    E08: Added global $ff, sjain2's changes, create users in init.rb
    git-svn-id: https://svn.csc.ncsu.edu:8443/svn/research-expertiza/projectB/trunk@1340 392d7b7b-3f31-0410-9dc3-c95a9635ea79

--- features/step_definitions/init.rb
+++ features/step_definitions/init.rb
@@ -1,6 +1,5 @@
 require 'firewatir'
 require 'capybara/cucumber'
-#Capybara.app = "make sure this isnt nil"
 
 # patch to make faster typing in text fields
 module FireWatir
@@ -29,3 +28,97 @@ module FireWatir
         end
       end
     end
+
+### Boot up firefox
+  $ff = FireWatir::Firefox.new
+  $ff.goto("http://localhost:3000")
+
+  # if already logged in, log us out
+  if($ff.button(:value, "Logout").exists?)
+    $ff.button(:value, "Logout").click
+  end
+
+# log in as admin
+  $ff.text_field(:name,"login[name]").set('admin')
+  $ff.text_field(:name,"login[password]").set('admin')
+  $ff.button(:value,"Login").click
+
+# admin logged in, now, to check and add users
+  $ff.link(:text, 'Manage...').fire_event('onmouseover')
+  $ff.link(:text, 'Users').click
+
+# create users: BTW -> we're doing this since 
+# fixtures is horrendously broken and we don't have
+# time to fix it ourselves.
+# if nothing else, this proves a good test to create
+# users by hand
+
+  $ff.link(:text, 'Manage...').fire_event('onmouseover')
+  $ff.link(:text, 'Users').click
+
+# Gehringer
+  if($ff.link(:text, 'g').exists?)
+    $ff.link(:text, 'g').click
+  end
+  if(!$ff.link(:text, 'Gehringer').exists?)
+    $ff.link(:text, 'New User').click
+    $ff.select_list(:name,'user[role_id]').select('Instructor')
+    $ff.text_field(:name,'user[name]').set('Gehringer')
+    $ff.text_field(:name,'user[clear_password]').set('gehringer')
+    $ff.text_field(:name,'user[confirm_password]').set('gehringer')
+    $ff.button(:name, 'commit').click
+  end
+
+# Titus
+  if($ff.link(:text, 't').exists?)
+    $ff.link(:text, 't').click
+  end
+  if(!$ff.link(:text, 'Titus').exists?)
+    $ff.link(:text, 'New User').click
+    $ff.select_list(:name,'user[role_id]').select('Teaching Assistant')
+    $ff.text_field(:name,'user[name]').set('Titus')
+    $ff.text_field(:name,'user[clear_password]').set('titus')
+    $ff.text_field(:name,'user[confirm_password]').set('titus')
+    $ff.button(:name, 'commit').click
+  end
+
+# mtreece
+  if($ff.link(:text, 'm').exists?)
+    $ff.link(:text, 'm').click
+  end
+  if(!$ff.link(:text, 'mtreece').exists?)
+    $ff.link(:text, 'New User').click
+    $ff.select_list(:name,'user[role_id]').select('Student')
+    $ff.text_field(:name,'user[name]').set('mtreece')
+    $ff.text_field(:name,'user[clear_password]').set('mtreece_pw')
+    $ff.text_field(:name,'user[confirm_password]').set('mtreece_pw')
+    $ff.button(:name, 'commit').click
+  end
+
+
+# sjain2
+  if($ff.link(:text, 's').exists?)
+    $ff.link(:text, 's').click
+  end
+  if(!$ff.link(:text, 'sjain2').exists?)
+    $ff.link(:text, 'New User').click
+    $ff.select_list(:name,'user[role_id]').select('Student')
+    $ff.text_field(:name,'user[name]').set('sjain2')
+    $ff.text_field(:name,'user[clear_password]').set('sjain2_pw')
+    $ff.text_field(:name,'user[confirm_password]').set('sjain2_pw')
+    $ff.button(:name, 'commit').click
+  end
+
+
+# nobody
+  if($ff.link(:text, 'n').exists?)
+    $ff.link(:text, 'n').click
+  end
+  if(!$ff.link(:text, 'nobody').exists?)
+    $ff.link(:text, 'New User').click
+    $ff.select_list(:name,'user[role_id]').select('Student')
+    $ff.text_field(:name,'user[name]').set('nobody')
+    $ff.text_field(:name,'user[clear_password]').set('nobody_pw')
+    $ff.text_field(:name,'user[confirm_password]').set('nobody_pw')
+    $ff.button(:name, 'commit').click
+  end
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,92 +1,100 @@
 require 'firewatir'
 require 'capybara/cucumber'
 
-ff=FireWatir::Firefox.new
+#$ff=FireWatir::Firefox.new
 
 Given /^"([^"]*)":"([^"]*)" logs into the system/ do |user_name, user_pw|
-  ff.goto("http://localhost:3000")
+  $ff.goto("http://localhost:3000")
 
   # if already logged in, log us out
-  if(ff.button(:value, "Logout").exists?)
-    ff.button(:value, "Logout").click
+  if($ff.button(:value, "Logout").exists?)
+    $ff.button(:value, "Logout").click
   end
 
-  ff.text_field(:name,"login[name]").set(user_name)
-  ff.text_field(:name,"login[password]").set(user_pw)
-  ff.button(:value,"Login").click
+  $ff.text_field(:name,"login[name]").set(user_name)
+  $ff.text_field(:name,"login[password]").set(user_pw)
+  $ff.button(:value,"Login").click
 end
 
-Given /^will create an assignment named "([^"]*)"$/ do |assignment_name|
+Given /^user will create an assignment named "([^"]*)"$/ do |assignment_name|
   # make an arbitrary assignment object in the system
   # with non-important settings...
 
   # if doesn't have "Manage...", fail immediately
-  if(!ff.contains_text "Manage...")
+  if(!$ff.contains_text "Manage...")
     fail "I cannot find the \"Manage...\" link!"
   end
-  ff.link(:text, "Manage...").click
-  ff.link(:text, "Create Public Assignment").click
+  $ff.link(:text, "Manage...").click
+  $ff.link(:text, "Create Public Assignment").click
   
-  ff.text_field(:name,"assignment[name]").set(assignment_name)
-  ff.text_field(:name,"assignment[directory_path]").set("td")
-  ff.text_field(:name,"assignment[spec_location]").set("xyz")  
+  $ff.text_field(:name,"assignment[name]").set(assignment_name)
+  $ff.text_field(:name,"assignment[directory_path]").set("td")
+  $ff.text_field(:name,"assignment[spec_location]").set("xyz")  
 
-  ff.select_list(:name,"assignment[wiki_type_id]").select("No")
-  ff.select_list(:name,"assignment[team_assignment]").select("Yes")
-  ff.select_list(:name,"assignment[staggered_deadline]").select("No")
- #ff.text_field(:name,"assignment[codereview]").value = ("false")
- #ff.text_field(:name,"assignment[team_assignment]").value = ("true")
- #ff.text_field(:name,"assignment[staggered_deadline]").value = ("true")
+  $ff.select_list(:name,"assignment[wiki_type_id]").select("No")
+  $ff.select_list(:name,"assignment[team_assignment]").select("Yes")
+  $ff.select_list(:name,"assignment[staggered_deadline]").select("No")
+ #$ff.text_field(:name,"assignment[codereview]").value = ("false")
+ #$ff.text_field(:name,"assignment[team_assignment]").value = ("true")
+ #$ff.text_field(:name,"assignment[staggered_deadline]").value = ("true")
 
 
-  ff.text_field(:name,"weights[selfassessment]").set("10")
-  ff.text_field(:name,"limits[selfassessment]").set("30")
+  $ff.text_field(:name,"weights[selfassessment]").set("10")
+  $ff.text_field(:name,"limits[selfassessment]").set("30")
  
-  ff.text_field(:name,"weights[review]").set("15")
-  ff.text_field(:name,"limits[review]").set("25")
+  $ff.text_field(:name,"weights[review]").set("15")
+  $ff.text_field(:name,"limits[review]").set("25")
 
-  ff.text_field(:name,"weights[metareview]").set("35")
-  ff.text_field(:name,"limits[metareview]").set("45")
+  $ff.text_field(:name,"weights[metareview]").set("35")
+  $ff.text_field(:name,"limits[metareview]").set("45")
  
-  ff.text_field(:name,"weights[feedback]").set("35")
-  ff.text_field(:name,"limits[feedback]").set("45")
+  $ff.text_field(:name,"weights[feedback]").set("35")
+  $ff.text_field(:name,"limits[feedback]").set("45")
  
-  ff.text_field(:name,"weights[supervisor]").set("35")
-  ff.text_field(:name,"limits[supervisor]").set("45")
+  $ff.text_field(:name,"weights[supervisor]").set("35")
+  $ff.text_field(:name,"limits[supervisor]").set("45")
  
-  ff.text_field(:name,"weights[reader]").set("35")
-  ff.text_field(:name,"limits[reader]").set("45")
+  $ff.text_field(:name,"weights[reader]").set("35")
+  $ff.text_field(:name,"limits[reader]").set("45")
  
-  ff.text_field(:name,"assignment[dynamic_reviewer_response_time_limit_hours]").set("48")
- #ff.text_field(:name,"assignment[dynamic_reviewer_response_time_limit_hours]").set("48")
-  ff.text_field(:name,"submit_deadline[due_at]").set("2010-12-18 01:59:32")
-  ff.text_field(:name,"review_deadline[due_at]").set("2010-12-28 01:59:32")
-  ff.text_field(:name,"switch_deadline[due_at]").set("2011-01-03 01:59:32")
+  $ff.text_field(:name,"assignment[dynamic_reviewer_response_time_limit_hours]").set("48")
+  $ff.text_field(:name,"submit_deadline[due_at]").set("2010-12-18 01:59:32")
+  $ff.text_field(:name,"review_deadline[due_at]").set("2010-12-28 01:59:32")
+  $ff.text_field(:name,"switch_deadline[due_at]").set("2011-01-03 01:59:32")
 
-  ff.select_list(:name,"submit_deadline[submission_allowed_id]").select("No")
-  ff.select_list(:name,"review_deadline[submission_allowed_id]").select("OK")
-  ff.select_list(:name,"reviewofreview_deadline[submission_allowed_id]").select("Late")
-  ff.select_list(:name,"switch_deadline[submission_allowed_id]").select("OK")
+  $ff.select_list(:name,"submit_deadline[submission_allowed_id]").select("No")
+  $ff.select_list(:name,"review_deadline[submission_allowed_id]").select("OK")
+  $ff.select_list(:name,"reviewofreview_deadline[submission_allowed_id]").select("Late")
+  $ff.select_list(:name,"switch_deadline[submission_allowed_id]").select("OK")
 
 
-  ff.select_list(:name,"submit_deadline[threshold]").select("16")
-  ff.button(:name, "save").click #hit create
+  $ff.select_list(:name,"submit_deadline[threshold]").select("16")
+  $ff.button(:name, "save").click #hit create
   
 end
 
 
 Then /^the assignment named "([^"]*)" will exist$/ do |assignment_name|
-  if(!ff.contains_text "Manage...")
+  if(!$ff.contains_text "Manage...")
     fail "I cannot find the \"Manage...\" link!"
   end
-  ff.link(:text, "Manage...").click
-  ff.link(:name, '3_3Link').click
-  assert(ff.contains_text assignment_name)
+  $ff.link(:text, "Manage...").click
+  $ff.link(:name, '3_3Link').click
+  assert($ff.contains_text assignment_name)
 end
 
 
 Given /^user "([^"]*)" is a participant of "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+ 
+  if(!$ff.contains_text "Manage...")
+    fail "I cannot find the \"Manage...\" link!"
+  end
+  
+  $ff.link(:text, "Manage...").click
+ 
+  $ff.link(:id, "3_3Link").fire_event("onClick")
+ #Add participant 
+  $ff.link(:text, "Add/remove participants").click
 end
 
 Given /^user "([^"]*)" is a reviewer of "([^"]*)" for "([^"]*)"/ do |arg1, arg2, arg3|
@@ -105,20 +113,63 @@ Then /^review of "([^"]*)" for "([^"]*)" by "([^"]*)" will not exist$/ do |arg1,
   pending # express the regexp above with the code you wish you had
 end
 
-When /^user adds "([^"]*)" to the assignment, "([^"]*)"/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+#AddParticipant
+
+#Given /^the assignment named "([^"]*)" will exist$/ do |assignment_name|
+  #check if the assignment is created
+ # $ff.link(:text, "Manage...").click
+ # $ff.link(:name, '3_3Link').click
+ # if(!$ff.contains_text assignment_name)
+ #    Given user will create an assignment named  assignment_name
+ # end
+# end
+
+When /^user adds "([^"]*)" to the assignment, "([^"]*)"/ do |user_name, assignment_name|
+ #TODO
+   assignment_xpath = "//td[]text()=#{assingment_name}"
+   assignment_name_elem = element_by_xpath(assignment_xpath)
+   assign_add_participant_link = element_by_xpath(following-sibling::assignment_xpath/li[4]/a/@href)
+   $ff.link(:href, assign_add_participant_link).click
+   
+   #add username
+   $ff.text_field(:name,"user[name]").set(user_name)  
+   $ff.button(:value, "Add Participant").click
+   
 end
 
 Then /^"([^"]*)" will be a participant of "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+ #TODO
 end
 
-Given /^will create a course named "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+#adding a course
+
+Given /^will create a course named "([^"]*)"$/ do |course_name|
+   # make an arbitrary assignment object in the system
+  # with non-important settings...
+
+  # if doesn't have "Manage...", fail immediately
+  if(!$ff.contains_text "Manage...")
+    fail "I cannot find the \"Manage...\" link!"
+  end
+  $ff.link(:text, "Manage...").click
+  $ff.link(:text, "Create Public Course").click
+  
+  $ff.text_field(:name,"course[name]").set(course_name)
+  $ff.text_field(:name,"course[directory_path]").set("td")
+  $ff.text_field(:name,"course[info]").set("test info")  
+
+  $ff.button(:name, "commit").click #hit create
+  
 end
 
-Then /^the course named "([^"]*)" will exists$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^the course named "([^"]*)" will exists$/ do |course_name|
+
+  if(!$ff.contains_text "Manage...")
+    fail "I cannot find the \"Manage...\" link!"
+  end
+  $ff.link(:text, "Manage...").click
+  $ff.link(:name, '2_2Link').click
+  assert($ff.contains_text course_name)
 end
 
 Given /^user "([^"]*)" scored (\d+) on assignment "([^"]*)"$/ do |arg1, arg2, arg3|
@@ -155,5 +206,6 @@ Then /^the assignment named "([^"]*)" will not exist$/ do |arg1|
 end
 
 Then /^user has logged in$/ do
-  assert(ff.button(:value, "Logout").exists?)
+  assert($ff.button(:value, "Logout").exists?)
 end
+
